79ae793c6852113a3329a92ce67a3b8e31a7a1bb
Albert Garde <albertsgarde@gmail.com>
2023-12-17 19:31:20 +0100

    :recycle: Move `NeuronStore` to separate file

--- src/n2g/__init__.py
+++ src/n2g/__init__.py
@@ -1,4 +1,11 @@
 from .word_tokenizer import WordTokenizer, sentence_tokenizer
 from .fast_augmenter import FastAugmenter, augment
+from .neuron_store import NeuronStore
 
-__all__ = ["WordTokenizer", "sentence_tokenizer", "FastAugmenter", "augment"]
+__all__ = [
+    "WordTokenizer",
+    "sentence_tokenizer",
+    "FastAugmenter",
+    "augment",
+    "NeuronStore",
+]
--- src/script.py
+++ src/script.py
@@ -29,7 +29,7 @@ from sklearn.metrics import classification_report
 from graphviz import Digraph, escape
 from IPython.display import Image, display
 
-from .n2g import FastAugmenter, WordTokenizer
+from .n2g import FastAugmenter, WordTokenizer, NeuronStore
 import n2g
 
 parser = re.compile('\{"tokens": ')
@@ -1161,129 +1161,6 @@ def get_summary_stats(path, verbose=True):
     return summary_stats
 
 
-class NeuronStore:
-    def __init__(self, path):
-        if not os.path.exists(path):
-            neuron_store = {"activating": {}, "important": {}}
-            with open(path, "w") as ofh:
-                json.dump(neuron_store, ofh, indent=2, ensure_ascii=False)
-
-        with open(path) as ifh:
-            self.store = json.load(ifh)
-
-        self.to_sets()
-        self.path = path
-        self.count_tokens()
-        self.by_neuron()
-
-    def save(self):
-        self.to_lists()
-        with open(self.path, "w") as ofh:
-            json.dump(self.store, ofh, indent=2, ensure_ascii=False)
-        self.to_sets()
-
-    def to_sets(self):
-        self.store = {
-            token_type: {token: set(info) for token, info in token_dict.items()}
-            for token_type, token_dict in self.store.items()
-        }
-
-    def to_lists(self):
-        self.store = {
-            token_type: {token: list(set(info)) for token, info in token_dict.items()}
-            for token_type, token_dict in self.store.items()
-        }
-
-    def by_neuron(self):
-        self.neuron_to_tokens = {}
-        for token_type, token_dict in self.store.items():
-            for token, neurons in token_dict.items():
-                for neuron in neurons:
-                    if neuron not in self.neuron_to_tokens:
-                        self.neuron_to_tokens[neuron] = {
-                            "activating": set(),
-                            "important": set(),
-                        }
-                    self.neuron_to_tokens[neuron][token_type].add(token)
-
-    def search(self, tokens_and_types):
-        match_arr = []
-
-        for token, token_type in tokens_and_types:
-            token_types = (
-                [token_type] if token_type is not None else ["activating", "important"]
-            )
-            token_matches = set()
-
-            for token_type in token_types:
-                matches = self.store[token_type].get(token, set())
-                token_matches |= matches
-
-            match_arr.append(token_matches)
-
-        valid_matches = set.intersection(*match_arr)
-        return valid_matches
-
-    def count_tokens(self):
-        self.neuron_individual_token_counts = defaultdict(Counter)
-        self.neuron_total_token_counts = Counter()
-        for token_type, token_dict in self.store.items():
-            for token, neurons in token_dict.items():
-                for neuron in neurons:
-                    self.neuron_individual_token_counts[neuron][token] += 1
-                    self.neuron_total_token_counts[neuron] += 1
-
-    def find_similar(self, target_token_types=None, threshold=0.9):
-        if target_token_types is None:
-            target_token_types = {"activating", "important"}
-
-        similar_pairs = []
-        subset_pairs = []
-
-        for i, (neuron_1, neuron_dict_1) in enumerate(self.neuron_to_tokens.items()):
-            if i % 1000 == 0:
-                print(
-                    f"{i} of {len(self.neuron_to_tokens.items())} complete", flush=True
-                )
-
-            for j, (neuron_2, neuron_dict_2) in enumerate(
-                self.neuron_to_tokens.items()
-            ):
-                if i <= j:
-                    continue
-
-                all_similar = []
-                all_subset = []
-
-                for token_type in target_token_types:
-                    length_1 = len(neuron_dict_1[token_type])
-                    length_2 = len(neuron_dict_2[token_type])
-
-                    intersection = neuron_dict_1[token_type] & neuron_dict_2[token_type]
-                    similar = (
-                        len(intersection) / max(length_1, length_2, 1)
-                    ) >= threshold
-                    subset = (
-                        len(intersection) / max(min(length_1, length_2), 1) >= threshold
-                    )
-
-                    all_similar.append(similar)
-                    all_subset.append(subset)
-
-                if all(all_similar):
-                    similar_pairs.append((neuron_1, neuron_2))
-                elif all(all_subset):
-                    # The first token indicates the superset neuron and the second the subset neuron
-                    subset_pair = (
-                        (neuron_1, neuron_2)
-                        if length_2 < length_1
-                        else (neuron_2, neuron_1)
-                    )
-                    subset_pairs.append(subset_pair)
-
-        return similar_pairs, subset_pairs
-
-
 def view_neuron(path):
     display(Image(filename=path))
 
@@ -2313,14 +2190,14 @@ if __name__ == "__main__":
     aug_model = AutoModelForMaskedLM.from_pretrained(aug_model_checkpoint).to(device)
     aug_tokenizer = AutoTokenizer.from_pretrained(aug_model_checkpoint)
 
-
-
     with open(f"{base_path}/data/word_to_casings.json", encoding="utf-8") as ifh:
         word_to_casings = json.load(ifh)
 
     stick_tokens = {"'"}
     word_tokenizer = WordTokenizer(set(), stick_tokens)
-    fast_aug = FastAugmenter(aug_model, aug_tokenizer, word_tokenizer, model, word_to_casings)
+    fast_aug = FastAugmenter(
+        aug_model, aug_tokenizer, word_tokenizer, model, word_to_casings
+    )
 
     # main()
 
