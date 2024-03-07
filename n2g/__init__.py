dc6faba6fff661a5cb667b0e697945d23b12b092
Albert Garde <albertsgarde@gmail.com>
2024-03-07 18:02:04 +0100

    :sparkles: Add more things to public interface

--- n2g/__init__.py
+++ n2g/__init__.py
@@ -1,3 +1,6 @@
+from .neuron_model import NeuronModel
 from .run import TrainConfig, run, run_layer
+from .stats import NeuronStats
+from .tokenizer import Tokenizer
 
-__all__ = ["TrainConfig", "run", "run_layer"]
+__all__ = ["TrainConfig", "run", "run_layer", "Tokenizer", "NeuronModel", "NeuronStats"]
