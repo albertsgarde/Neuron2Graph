e96457caf27d29864cd36cd3a852103d079b1130
Albert Garde <albertsgarde@gmail.com>
2024-03-07 11:53:45 +0100

    :sparkles: Remove most things from __all__

--- n2g/__init__.py
+++ n2g/__init__.py
@@ -1,25 +1,3 @@
-from .augmenter import AugmentationConfig, Augmenter, WordToCasings, augment
-from .evaluate import evaluate
-from .fit import FitConfig, ImportanceConfig, PruneConfig, fit_neuron_model
-from .neuron_model import NeuronModel
-from .neuron_store import NeuronStore
-from .run import TrainConfig, run, run_training
-from .scrape import scrape_neuroscope_samples
+from .run import TrainConfig, run, run_layer
 
-__all__ = [
-    "Augmenter",
-    "augment",
-    "NeuronStore",
-    "NeuronModel",
-    "fit_neuron_model",
-    "evaluate",
-    "run_training",
-    "run",
-    "scrape_neuroscope_samples",
-    "FitConfig",
-    "ImportanceConfig",
-    "PruneConfig",
-    "AugmentationConfig",
-    "TrainConfig",
-    "WordToCasings",
-]
+__all__ = ["TrainConfig", "run", "run_layer"]
