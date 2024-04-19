f9bacca7cb3ba239d3e0a551ba6113c004d47e3c
Albert Garde <albertsgarde@gmail.com>
2024-04-19 16:15:48 +0200

    :sparkles: Make config classes public

--- n2g/__init__.py
+++ n2g/__init__.py
@@ -1,6 +1,19 @@
+from .fit import AugmentationConfig, FitConfig, ImportanceConfig, PruneConfig
 from .neuron_model import NeuronModel
 from .run import TrainConfig, feature_activation, run, run_layer
 from .stats import NeuronStats
 from .tokenizer import Tokenizer
 
-__all__ = ["TrainConfig", "run", "run_layer", "Tokenizer", "NeuronModel", "NeuronStats", "feature_activation"]
+__all__ = [
+    "TrainConfig",
+    "run",
+    "run_layer",
+    "Tokenizer",
+    "NeuronModel",
+    "NeuronStats",
+    "feature_activation",
+    "FitConfig",
+    "PruneConfig",
+    "AugmentationConfig",
+    "ImportanceConfig",
+]
